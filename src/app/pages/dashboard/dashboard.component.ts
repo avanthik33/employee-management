4ba7ba9f1216e455161477a868dbb3659cc6b43c
avanthik <avanthik33@gmail.com>
2025-01-13 09:54:29 +0530

    changes made

--- src/app/pages/dashboard/dashboard.component.ts
+++ src/app/pages/dashboard/dashboard.component.ts
@@ -1,5 +1,4 @@
 import { Component } from '@angular/core';
-import { EmployeeComponent } from "../employee/employee.component";
 
 @Component({
   selector: 'app-dashboard',
--- src/app/pages/project/view-all-project/view-all-project.component.ts
+++ src/app/pages/project/view-all-project/view-all-project.component.ts
@@ -1,6 +1,5 @@
 import {
   Component,
-  effect,
   ElementRef,
   inject,
   OnInit,
